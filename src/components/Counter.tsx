0858bb10a32323c13f56e92a1b37e1bf34260ac8
John Rood <mr.john.rood@gmail.com>
2023-04-12 10:09:34 -0500

    test time ssr

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -3,9 +3,13 @@ import "./Counter.css";
 
 export default function Counter() {
   const [count, setCount] = createSignal(0);
+  const currentTime = new Date().toLocaleDateString('en-US', { timeZone: 'America/Chicago' });
   return (
-    <button class="increment" onClick={() => setCount(count() + 1)}>
-      Clicks: {count()}
-    </button>
+    <>
+      <button class="increment" onClick={() => setCount(count() + 1)}>
+        Clicks: {count()}
+      </button>
+      time: {currentTime}
+    </>
   );
 }
--- src/root.tsx
+++ src/root.tsx
@@ -13,7 +13,7 @@ import "./root.css";
 export default () => (
   <Html lang="en">
     <Head>
-      <Title>SolidStart - Bare</Title>
+      <Title>Kronmeter</Title>
       <Meta charset="utf-8" />
       <Meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
